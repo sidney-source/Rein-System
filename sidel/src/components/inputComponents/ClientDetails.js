62130a606751d1ed8f5627cd3ed52944884a4542
sidney-source <sidneymutai@gmail.com>
2022-06-10 10:09:33 +0300

    h

--- sidel/src/components/inputComponents/ClientDetails.js
+++ sidel/src/components/inputComponents/ClientDetails.js
@@ -1,7 +1,10 @@
-import { Grid, Paper, TextField } from "@mui/material";
+import { Button, Grid, Paper } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
-
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SaveTwoToneIcon from "@mui/icons-material/SaveTwoTone";
 export const ClientDetailsPage = (props) => {
    return (
       <Containerd>
@@ -21,37 +24,40 @@ export const ClientDetailsPage = (props) => {
                sx={{
                   p: 0.5,
                   display: "grid",
-                  gap: 1,
+                  gap: 2,
                }}
             >
-               <h1>Client Details</h1>
-               <TextField
-                  size="small"
-                  label="Insured"
-                  sx={{
-                     width: 300,
-                     height: 30,
-                     padding: 0,
-                  }}
-               ></TextField>
-               <TextField
-                  size="small"
-                  label="Insured"
-                  sx={{
-                     width: 300,
-                     height: 30,
-                     padding: 0,
-                  }}
-               ></TextField>{" "}
-               <TextField
-                  size="small"
-                  label="Insured"
-                  sx={{
-                     width: 300,
-                     height: 30,
-                     padding: 0,
-                  }}
-               ></TextField>
+               <DetailsCont>
+                  <h1>Client Details</h1>
+                  <BtnGrp1>
+                     <Button
+                        variant="contained"
+                        sx={{
+                           p: 1,
+                           display: "flex",
+                           height: 30,
+                           fontSize: 12,
+                        }}
+                        endIcon={<AddCircleOutlineIcon />}
+                     >
+                        Edit
+                     </Button>
+                     <IconButton
+                        aria-label="delete"
+                        color="success"
+                        size="small"
+                     >
+                        <DeleteIcon size="large" color="error" />
+                     </IconButton>
+                     <IconButton
+                        aria-label="delete"
+                        color="success"
+                        size="small"
+                     >
+                        <SaveTwoToneIcon color="success" />
+                     </IconButton>
+                  </BtnGrp1>
+               </DetailsCont>
             </Grid>
             <Grid
                sx={{
@@ -85,5 +91,17 @@ export const ClientDetailsPage = (props) => {
 const Containerd = styled.div`
    margin: 0px;
 `;
-
+const DetailsCont = styled.div`
+   display: flex;
+   align-items: baseline;
+   justify-content: space-between;
+   vertical-align: middle;
+`;
+const BtnGrp1 = styled.div`
+   display: flex;
+   vertical-align: middle;
+   align-items: center;
+   justify-content: center;
+   gap: 3px;
+`;
 export default ClientDetailsPage;
