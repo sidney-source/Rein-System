1f313009302c2865ec7127666f9dd062992a3237
sidney-source <66361619+sidney-source@users.noreply.github.com>
2023-05-16 13:07:57 +0000

    Home

--- sidel/src/components/Home.js
+++ sidel/src/components/Home.js
@@ -189,15 +189,17 @@ const Home = (props) => {
                 }}
               >
                 <a>
+                  Svg Code
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="22"
-                    height="22"
+                    width="16"
+                    height="16"
                     fill="currentColor"
-                    class="bi bi-signpost"
+                    class="bi bi-file-earmark-post-fill"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M7 1.414V4H2a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h5v6h2v-6h3.532a1 1 0 0 0 .768-.36l1.933-2.32a.5.5 0 0 0 0-.64L13.3 4.36a1 1 0 0 0-.768-.36H9V1.414a1 1 0 0 0-2 0zM12.532 5l1.666 2-1.666 2H2V5h10.532z" />
+                    {" "}
+                    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-5-.5H7a.5.5 0 0 1 0 1H4.5a.5.5 0 0 1 0-1zm0 3h7a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5z" />{" "}
                   </svg>
                 </a>
                 <h1>Quote</h1>
