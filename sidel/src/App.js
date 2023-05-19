9dd0a9e37f060da74bc9ad392197b9fbd8f1449a
sidney-source <sidneymutai@gmail.com>
2023-05-19 16:32:52 +0300

    h

--- sidel/src/App.js
+++ sidel/src/App.js
@@ -7,6 +7,18 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
+
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("applistening on port 3000");
+});
+
+app.get("/home", (req, res) => {
+  res.json({ mssg: "welcome to teh api" });
+});
 function App() {
   return (
     <div className="App">
