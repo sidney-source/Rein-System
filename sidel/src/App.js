cb7a2e20989f2961f3d45ab4b11f0d8b94452c8d
sidney-source <sidneymutai@gmail.com>
2023-05-22 16:47:52 +0300

    j

--- sidel/src/App.js
+++ sidel/src/App.js
@@ -9,9 +9,17 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 
 const express = require("express");
-
+const mongoose = require("");
 const app = express();
 
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://SidMut:Password123@reinqt.1iutv8u.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
 app.listen(3000, () => {
   console.log("applistening on port 3000");
 });
@@ -19,6 +27,7 @@ app.listen(3000, () => {
 app.get("/home", (req, res) => {
   res.json({ mssg: "welcome to teh api" });
 });
+
 function App() {
   return (
     <div className="App">
