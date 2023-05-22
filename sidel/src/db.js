fe81804dc147bbc77c5ccd46a9bbfc34ac1826da
sidney-source <sidneymutai@gmail.com>
2023-05-22 16:47:59 +0300

    h

--- sidel/src/db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require("mongodb");
-
-module.exports = {
-  connectToDb: () => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore");
-  },
-  getDb: () => {},
-};
